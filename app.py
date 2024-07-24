d7791ae47bdc490bbc9e15391b17cae0815e3bf2
SouvikUMD <87155394+SouvikUMD@users.noreply.github.com>
2024-07-24 10:59:49 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ Created on Fri Dec 22 13:05:54 2023
 from flask import Flask, request, render_template
 app = Flask(__name__)
 
+@app.route('/')
+def gfg_form():
+    return render_template("index.html")
 
 # A decorator used to tell the application
 # which URL is associated function
@@ -16,7 +19,8 @@ def gfg():
     if request.method == "POST":
        # getting input with name = fname in HTML form
        first_name = request.form.get("fname")
+       print("first_name: ", first_name)
        # getting input with name = lname in HTML form
        last_name = request.form.get("lname")
+       print("last_name: ", last_name)
        return "Your name is "+first_name + last_name
-    return render_template("index.html")
